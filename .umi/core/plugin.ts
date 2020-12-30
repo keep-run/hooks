bcd0dfd44337ec07ab0e9ea145dfef0edb27e629
王治春 <wangzhichun001@ke.com>
2020-12-30 14:41:56 +0800

    fix: delete .umi files

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/wangzhichun/Desktop/study/myHook/hooks/node_modules/_@umijs_runtime@3.3.3@@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
