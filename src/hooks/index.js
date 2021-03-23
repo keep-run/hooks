715200ac7a0b9931c98d232033bf66947bd2c2e5
王治春 <wangzhichun001@ke.com>
2021-03-23 21:52:26 +0800

    feat:整理目录

---
+++ src/hooks/index.js
@@ -0,0 +1,10 @@
+import useBoolean from './useBoolean'
+import useLocalStorage from './useLocalStorage'
+import useInterval from './useInterval'
+
+// 3333
+export {
+  useBoolean,
+  useLocalStorage,
+  useInterval
+}
